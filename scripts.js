992ea29949da65416b85e39d75838b393ea37a04
Kasia Kowalska <k.kowalska68@yahoo.com>
2018-02-03 20:11:04 +0100

    Modify js

--- scripts.js
+++ scripts.js
@@ -8,8 +8,8 @@ span.each(function(index, element) {
 $("span:odd").css('color', 'green');
 var allParagraphes = $('p');
 allParagraphes.each(function(index, element) {
-	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    	$(element).append(button)
+	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    	$(element).append(button);
 });
 
 $("button").click(function(){
